9f3cdf7d41dd67d043d51ff8d3d5da7f3761c5d1
Bojan <bojan.cestnik@temida.si>
2023-11-12 19:49:58 +0100

    added hacker and victim scripts 101

---
+++ kali-hacking/kali/hacker_101.py
@@ -0,0 +1,19 @@
+import socket
+
+if __name__ == "__main__":
+    hacker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    hacker_IP = "192.168.0.156" # kali linux IP
+    hacker_Port = 8008
+
+    socket_address = (hacker_IP, hacker_Port)
+    hacker_socket.bind(socket_address)
+    hacker_socket.listen(5)
+    print("Listening for incoming connection requests ...")
+
+    hacker_socket, client_address = hacker_socket.accept()
+    message = "This is a message from hacker."
+    message_bytes = message.encode()
+    hacker_socket.send(message_bytes)
+    print("Message from hacker sent")
+    hacker_socket.close()
