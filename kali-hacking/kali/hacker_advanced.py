fc1fbcb2d1a0807d9e19a79fb4de0847a5b10453
Bojan <bojan.cestnik@temida.si>
2023-11-12 21:26:41 +0100

    advanced 101

--- kali-hacking/kali/hacker_advanced.py
+++ kali-hacking/kali/hacker_advanced.py
@@ -10,6 +10,7 @@ def receive_file():
 
 if __name__ == "__main__":
     hacker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    hacker_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     hacker_IP = "192.168.64.111" # kali linux IP
     hacker_port = 8008
     socket_address = (hacker_IP, hacker_port)
