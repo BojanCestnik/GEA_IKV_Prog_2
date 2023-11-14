abe2375fc613043de417789633c850b6fb4296bd
Bojan <bojan.cestnik@temida.si>
2023-11-14 13:40:34 +0100

    changed IP

--- kali-hacking/kali/scapy_traceroute.py
+++ kali-hacking/kali/scapy_traceroute.py
@@ -1,9 +1,10 @@
 from scapy.all import *
 
-host = "45.33.32.156"
+# host_IP = "45.33.32.156"
+host_IP = "95.216.138.14"
 
 for i in range(1, 20):
-    packet = IP(dst=host, ttl=i) / UDP(dport=33434)
+    packet = IP(dst=host_IP, ttl=i) / UDP(dport=33434)
     # Send the packet and get a reply
     reply = sr1(packet, verbose=0,timeout=1)
     if reply is None:
